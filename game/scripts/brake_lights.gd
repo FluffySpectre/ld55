b19c3cd4ce2d6cdef71e36457cf343ea485b4374
Sennio <b.bosse1991@gmail.com>
2024-04-16 22:45:13 +0200

    Some small improvements

--- game/scripts/brake_lights.gd
+++ game/scripts/brake_lights.gd
@@ -1,8 +1,8 @@
 extends Node3D
 
 @onready var car_controller: CarController = get_parent()
-@onready var brake_light_left = $BrakeLightLeft
-@onready var brake_light_right = $BrakeLightRight
+@onready var brake_light_left: Light3D = $BrakeLightLeft
+@onready var brake_light_right: Light3D = $BrakeLightRight
 @onready var brake_particles_left: GPUParticles3D = $BrakeParticlesLeft
 @onready var brake_particles_right: GPUParticles3D = $BrakeParticlesRight
 
@@ -12,14 +12,14 @@ func _process(_delta):
 		return
 	
 	if car_controller.brake > 0 && car_controller.linear_velocity.length() > 0.05:
-		brake_light_left.visible = true
-		brake_light_right.visible = true
+		brake_light_left.light_energy = 1.0
+		brake_light_right.light_energy = 1.0
 		
 		brake_particles_left.emitting = true
 		brake_particles_right.emitting = true
 	else:
-		brake_light_left.visible = false
-		brake_light_right.visible = false
+		brake_light_left.light_energy = 0.3
+		brake_light_right.light_energy = 0.3
 		
 		brake_particles_left.emitting = false
 		brake_particles_right.emitting = false
