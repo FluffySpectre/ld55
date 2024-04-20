4648e33fbf404f9ba6fabd6cfd08b33c27008bf0
Sennio <b.bosse1991@gmail.com>
2024-04-20 17:43:48 +0200

    Added first version of the intro

--- game/scripts/follow_target.gd
+++ game/scripts/follow_target.gd
@@ -1,7 +1,8 @@
 extends Node3D
 
 @export var target: Node3D
+@export var offset: Vector3 = Vector3.ZERO
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	global_position = target.global_position
+	global_position = target.global_position + offset
