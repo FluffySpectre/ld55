b437851ea550b22347c05caa255aec766ad584bb
Sennio <b.bosse1991@gmail.com>
2024-04-20 17:35:04 +0200

    Fixed track ending collisions (Made one big collider instead of a separate collider for each track part)

--- game/scripts/follow_target.gd
+++ game/scripts/follow_target.gd
@@ -1,15 +1,7 @@
 extends Node3D
 
 @export var target: Node3D
-@export var smooth_move = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if smooth_move:
-		smooth_move_to_target(delta)
-		return
-	
 	global_position = target.global_position
-
-func smooth_move_to_target(delta):
-	global_position = global_position.move_toward(target.global_position, delta * 2.0)
--- game/scripts/track_generator.gd
+++ game/scripts/track_generator.gd
@@ -5,10 +5,14 @@ class_name TrackGenerator extends Node3D
 
 signal on_track_spawned
 
+@onready var track_collider: CollisionShape3D = $TrackCollider/CollisionShape3D
+@onready var track_collider_particles: GPUParticlesCollisionBox3D = $TrackCollider/GPUParticlesCollisionBox3D
+
 var num_parts_to_load = 6
 var loaded_street_parts = []
 var last_out_connector: Node3D
 var rng = RandomNumberGenerator.new()
+var first_frame = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,7 +20,11 @@ func _ready():
 
 func _process(_delta):
 	generate()
-	destroy_out_of_screen_street_parts()
+	
+	if !first_frame:
+		destroy_out_of_screen_street_parts()
+		
+	first_frame = false
 
 func get_street_part_scene():
 	return street_part_scenes[rng.randi_range(0, street_part_scenes.size() - 1)]
@@ -35,6 +43,9 @@ func spawn_street_part():
 	# Get connector for next street piece
 	last_out_connector = instance.get_node("Connector") as Node3D
 	
+	# Increase the dimensions of the track collider
+	resize_track_collider()
+	
 	# Emit track spawned signal
 	on_track_spawned.emit()
 
@@ -45,7 +56,7 @@ func generate():
 func destroy_out_of_screen_street_parts():
 	# Iterate (backwards) over all loaded street parts and destroy the parts which
 	# are behind the player and out of screen
-	for i in range(loaded_street_parts.size()-1,0,-1):
+	for i in range(loaded_street_parts.size()-1,-1,-1):
 		var part = loaded_street_parts[i] as Node3D
 		var on_screen_notifier = part.get_node("VisibleOnScreenNotifier3D") as VisibleOnScreenNotifier3D
 		var is_on_screen = on_screen_notifier.is_on_screen()
@@ -54,6 +65,8 @@ func destroy_out_of_screen_street_parts():
 		if !is_on_screen && is_behind_player:
 			loaded_street_parts.pop_at(i)
 			part.queue_free()
+	
+	resize_track_collider()
 
 func reset_track():
 	# Destroy all loaded street parts first
@@ -66,3 +79,38 @@ func reset_track():
 	rng = RandomNumberGenerator.new()
 	
 	generate()
+
+func resize_track_collider():
+	# Get all loaded track parts (children of this node) in child order
+	var track_parts = get_children()
+	
+	# Get the first and last track parts out of the list
+	# First child is the TrackCollider, so ignore this
+	var first_part = track_parts[1]
+	var last_part = track_parts[track_parts.size() - 1]
+	
+	# Calculate the distance between the global_position of the first
+	# track part and the connectors global_position of the last track part
+	# Assuming 'connector' is a node that represents the connection point
+	var first_pos = first_part.global_position
+	var last_pos = last_part.get_node("Connector").global_position
+	var distance = first_pos.distance_to(last_pos)
+	
+	# Update the collider sizes
+	# Set the z-Part of the size of the track collider to the calculated distance value
+	var collider_size = track_collider.shape.size
+	collider_size.z = distance
+	track_collider.shape.size = collider_size
+
+	# Adjust the position of the collider to be centered between the first and last parts
+	var new_position = first_pos.lerp(last_pos, 0.5)
+	track_collider.global_position = new_position
+	
+	# Set the z-Part of the size of the track collider to the calculated distance value
+	collider_size = track_collider_particles.size
+	collider_size.z = distance
+	track_collider_particles.size = collider_size
+
+	# Adjust the position of the collider to be centered between the first and last parts
+	track_collider_particles.global_position = new_position
+
