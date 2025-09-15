4fa8ab0fb36e9160ab142986bf2bcbfe22b31818
Sennio <b.bosse1991@gmail.com>
2025-09-15 19:02:11 +0200

    Improved intro sequence

--- game/scripts/play_animation_on_enter.gd
+++ game/scripts/play_animation_on_enter.gd
@@ -2,6 +2,9 @@ class_name PlayAnimationOnEnter extends Node
 
 @export var animation_player: AnimationPlayer
 @export var animation_name: String = ""
+@export var trigger_once: bool = false
+
+var triggered: bool = false
 
 func _ready() -> void:
   var area = get_parent() as Area3D
@@ -10,4 +13,9 @@ func _ready() -> void:
 func on_body_entered(body: Node3D) -> void:
   if body != Globals.player_car:
     return
+    
+  if trigger_once && triggered:
+    return
+    
   animation_player.play(animation_name)
+  triggered = true
